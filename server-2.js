feb3e6b403adb18472fb8509a0abb4123837d636
prtomnoi <wry.tomnoi1083@gmail.com>
2024-11-22 13:52:12 +0700

    update code

--- server-2.js
+++ server-2.js
@@ -72,7 +72,7 @@ wss.on('connection', (ws) => {
             const { channel_id, sender, message, type, seed } = parsedData;
 
             if (!channel_id || !sender || !type) {
-                console.error("Missing required properties in the received data.");
+                console.error(`Missing required properties in the received data.  channel_id : ${channel_id} and sender : ${sender} and message : ${type}`);
                 ws.send(JSON.stringify({ error: "Missing required properties." }));
                 return;
             }
